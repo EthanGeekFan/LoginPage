4b4d75a51e264c7c673c4eb36cbbcabf8401dadc
Yifan Yang <1195767760@qq.com>
2020-10-16 04:05:07 +0800

    first commit

---
+++ login/login.js
@@ -0,0 +1,10 @@
+// const signIn_span = document.getElementById("signIn");
+// signIn_span.addEventListener('click', function() {
+//     console.log("click");
+// })
+
+$(() => {
+    $('#signIn').on('click', () => {
+        console.log('switch to signin')
+    })
+})
